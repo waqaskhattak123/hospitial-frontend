939c0f82bf53a60d1a2efbb00bf52c095c9abd22
Waqas Ahmad <waqasapsacs@gmail.com>
2023-12-01 11:21:50 +0500

    umar

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
 // import Navbar from "../components/Navbar";
 // import TopCharacterOne from "../components/TopCharacterOne";
 // import WhoWeAre from "../components/WhoWeAre";
@@ -11,7 +16,7 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-{/*       
+      {/*       
       <BrowserRouter>
         <Routes>
           
