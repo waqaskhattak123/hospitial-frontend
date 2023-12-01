30df9dbe8b089333c393680d51c18af47b609d1f
Waqas Ahmad <waqasapsacs@gmail.com>
2023-12-01 12:05:31 +0500

    contact changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,38 +1,8 @@
 import React from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.init();
-
-// import Navbar from "../components/Navbar";
-// import TopCharacterOne from "../components/TopCharacterOne";
-// import WhoWeAre from "../components/WhoWeAre";
-// import OurDoctors from "../components/OurDoctors";
-// import SendUsMessage from "../components/SendUsMessage";
-// import Footer from "../components/Footer";
-// import Registeration from "../components/Registeration";
-// import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 const LandingPage = () => {
-  return (
-    <div>
-      {/*       
-      <BrowserRouter>
-        <Routes>
-          
-          <Route path="/navbar" element={<Navbar />} />
-          <Route path="/topchartone" element={<TopCharacterOne />} />
-          <Route path="/whoerare" element={<WhoWeAre />} />
-          <Route path="/ourdoctors" element={<OurDoctors />} />
-          <Route path="/sendusmessage" element={<SendUsMessage />} />
-          <Route path="/footer" element={<Footer />} />
-          <Route
-            path="/registeration"  element={<Registeration />}
-          ></Route>
-        </Routes>
-      </BrowserRouter> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default LandingPage;
